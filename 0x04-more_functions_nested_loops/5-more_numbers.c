7d2988fd3ad9d1dbbcc785dae69415ab0259a5e0
minlinx <minaproblemsolver@gmail.com>
2023-09-14 17:01:19 +0000

    This prints numbers 0 through 14 using the _putchar function

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers
+ *	from 0 to 14
+ */
+
+void more_numbers(void)
+{
+	char num;
+	int times;
+
+	for (times = 0; times <= 9; times++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+			_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
