294877bbd51714b21d77b7f526bf54971582e115
minlinx <minaproblemsolver@gmail.com>
2023-09-21 17:40:41 +0000

    This is a function that prints an integer.

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,25 +1,43 @@
 #include "main.h"
-
 /**
- * print_number - prints an integers
+ * print_number - prints number
+ * @n:integer to convert to character
  *
- * @n: the integer to be printed
  */
-
 void print_number(int n)
 {
-	unsigned int i;
+	unsigned int abs;
+	int mult = 1;
+	unsigned int abSCount;
+	int i;
+	int c = 0;
 
+	if (n == 0)
+	{
+		_putchar('0');
+	}
 	if (n < 0)
 	{
 		_putchar('-');
-		i = -n;
+		n += 1;
+		n *= -1;
+		n++;
 	}
-	else
+	abs = n;
+	abSCount = n;
+
+	while (abSCount > 0)
 	{
-		i = n;
+		abSCount /= 10;
+		c++;
 	}
-	if (i / 10)
-		print_number(i / 10);
-	_putchar(i % 10 + '0');
+	for (i = 0; i < c - 1; i++)
+		mult *= 10;
+
+	for (i = 0; i < c; i++)
+	{
+		_putchar((abs / mult) + '0');
+		abs = abs % mult;
+		mult /= 10;
+	}
 }
