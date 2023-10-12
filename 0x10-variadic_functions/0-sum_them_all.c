cc9b0389aa99e6b78802e6f737dd1eded1dd5ac8
minlinx <minaproblemsolver@gmail.com>
2023-10-12 15:15:06 +0000

    This is a function that returns the sum of all its parameters.

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,26 +1,26 @@
+#include <stdarg.h>
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - returns the sum of all its
- *	parameteres
- * @n: number of parameters
- * Return: The sum or 0 if n == 0
+ * sum_them_all - return sum when given unknown amount of arguments
+ * @n: integers to add
+ * Return: sum
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list sum;
-	unsigned int i, add = 0;
+	va_list valist;
+	unsigned int sum = 0, i;
 
+	/* validate valist and initialize */
 	if (n == 0)
 		return (0);
+	va_start(valist, n);
 
-	va_start(sum, n);
+	/* iterate through list, update sum, free list */
 	for (i = 0; i < n; i++)
-	{
-		add += va_arg(sum, int);
-	}
-	va_end(sum);
+		sum += va_arg(valist, int);
+	va_end(valist);
 
-	return (add);
+	return (sum);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,23 +1,23 @@
-#ifndef VARIADIC_H
-#define VARIADIC_H
+#ifndef VARIADIC_FUNCTIONS
+#define VARIADIC_FUNCTIONS
 
-#include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
 
-typedef struct formats
-{
-	char c;
-	void (*f)(va_list);
-} _struct;
-
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-int _putchar(char c);
-void _char(va_list args);
-void _int(va_list args);
-void _float(va_list args);
-void _str(va_list args);
+
+/**
+ * struct dt - struct
+ * @letter: letter signifying data type
+ * @func: function ptr
+ */
+typedef struct dt
+{
+	char letter;
+	void (*func)(va_list);
+} datatype;
+
 #endif
