699096dd9d26c8477479e65cff6a49345e0cbd61
minlinx <minaproblemsolver@gmail.com>
2023-10-19 12:13:40 +0000

    This prints all the elemements of a of a list_t list.

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,10 @@
-#ifndef LIST
-#define LIST
+#ifndef LISTS_H
+#define LISTS_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h> /* size_t, printf */
+#include <stdlib.h> /* malloc */
+#include <string.h> /* strdup */
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -11,6 +12,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for ALX project
  */
 typedef struct list_s
 {
@@ -19,10 +21,9 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-
+/* function prototypes for all files */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
-int _putchar(char c);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
