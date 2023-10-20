b68be5fd8fe5fe1f3f56b4daa4297389c5c5907e
minlinx <minaproblemsolver@gmail.com>
2023-10-20 00:56:18 +0000

    This function adds a new node at the end of a list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,39 +1,52 @@
 #include "lists.h"
 
 /**
- * add_node_end - adds a node at the end of the
- * list
- * @head: the beginning of the list
- * @str: the element
- * Return: the address of the new element
+ * _strlen - find string length
+ * @str: string
+ * Return: length
  */
+int _strlen(const char *str)
+{
+	int len;
 
+	for (len = 0; str[len] != '\0'; len++)
+		;
+	return (len);
+}
+
+/**
+ * add_node_end - add node to end of linked list
+ * @head: linked list
+ * @str: data for new node
+ * Return: address of new element, or NULL if failed
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	int i;
-	list_t *new, *current;
+	list_t *new_node, *tmp; /* create new node */
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
+	if (str == NULL) /* validate input */
+		return (NULL);
+	if (strdup(str) == NULL) /* check if malloc errored */
 		return (NULL);
 
-	new->str = strdup(str);
-	if (new->str == NULL)
-	{
-		free(new);
+	new_node = malloc(sizeof(list_t)); /* malloc for new node */
+	if (new_node == NULL)
 		return (NULL);
-	}
-	new->next = NULL;
 
-	if (*head == NULL)
-		*head = new;
+	new_node->str = strdup(str); /* set node values */
+	new_node->len = _strlen(str);
+	new_node->next = NULL;
+
+	if (*head == NULL) /* if no list nodes, set new_node to beginning */
+		*head = new_node;
 	else
 	{
-		current = *head;
-		for (i = 0; current->next != NULL; i++)
-			current = current->next;
+		tmp = *head;
 
-		current->next = new;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = new_node;
 	}
-	return (new);
+
+	return (new_node);
 }
